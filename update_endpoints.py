842e3408ac51663372e4e4232167425e857164da
sirouk <8901571+sirouk@users.noreply.github.com>
2024-12-23 12:39:51 -0500

    fetch and append rpc list

--- update_endpoints.py
+++ update_endpoints.py
@@ -1,9 +1,16 @@
 import sys
+import os
+import dotenv
 import requests
 
 
+
 # Tolerance of max height: fixed count integer
-balancer_tolerance = 500  # This is the fixed count integer tolerance
+
+# Load environment variables
+dotenv.load_dotenv()
+balancer_tolerance = os.getenv("BALANCER_TOLERANCE", 7)
+chain_id = os.getenv("CHAIN_ID", "namada.5f5de2dd1b88cba30586420")
 
 
 #nginx_config = "/etc/nginx/sites-available/rpc-load-balancer"
@@ -11,7 +18,23 @@ nginx_config = sys.argv[1]
 
 # Read endpoints from the adjacent file
 with open("endpoints.txt", "r") as f:
-    endpoints = [line.strip() for line in f.readlines() if not line.startswith('#')]
+    endpoints = [line.strip() for line in f.readlines() if line.strip() != '' and not line.startswith('#')]
+
+
+RPC_LIST = "https://raw.githubusercontent.com/Luminara-Hub/namada-ecosystem/refs/heads/main/user-and-dev-tools/mainnet/rpc.json"
+try:
+    response = requests.get(RPC_LIST, timeout=5)
+    data = response.json()
+    for rpc in data:
+        if rpc['RPC Address'] not in endpoints:
+            endpoints.append(rpc['RPC Address'])
+    #print(data)
+except Exception as e:
+    print(f"Error fetching data from {RPC_LIST}: {e}")
+
+#print(endpoints)
+#quit()
+
 
 
 # Fetch ledger_version from each endpoint
@@ -19,15 +42,17 @@ ledger_versions = {}
 for endpoint in endpoints:
     if len(endpoint) > 0:
         try:
-            response = requests.get(endpoint, timeout=5)
+            response = requests.get(f"{endpoint}/block", timeout=1)
             data = response.json()
-            ledger_version = int(data.get('ledger_version'))
-            if isinstance(ledger_version, int) and ledger_version > 0:  # Ensure ledger_version is an integer
-                ledger_versions[endpoint] = int(ledger_version)
+            rpc_chain_id = data.get('result', {}).get('block', {}).get('header', {}).get('chain_id', '')
+            if rpc_chain_id == chain_id:
+                ledger_version = int(data.get('result', {}).get('block', {}).get('header', {}).get('height', 0))
+                if isinstance(ledger_version, int) and ledger_version > 0:  # Ensure ledger_version is an integer
+                    ledger_versions[endpoint] = ledger_version
         except Exception as e:
             print(f"Error fetching data from {endpoint}: {e}")
 
-print(ledger_versions)
+#print(ledger_versions)
 #quit()
 
 # Find the highest ledger_version
